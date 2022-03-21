2f48514f80319dfecf58b1b623f1dc2de1e1cf43
networkdavit <lonecreation@gmail.com>
2022-03-21 23:59:07 +0400

    Changing to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_03_21_113533) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
